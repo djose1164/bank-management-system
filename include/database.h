2ee57ade0d082a27dff6d21245792e8dd48aa02b
Jose S. Daniel <djose1164@gmail.com>
2021-04-23 00:40:31 -0400

    Temp changes

--- include/database.h
+++ include/database.h
@@ -12,13 +12,12 @@
 
 // Librerias necesarias.
 #ifdef __WIN32
-#include "../include/sqlite3.h"
+#include "../lib/include/sqlite3.h"
 #else
 #include <sqlite3.h>
 #endif //! __WIN32
 #include <stdbool.h>
 #include <stddef.h>
-#include "../include/inventario.h"
 
 /**Struct donde se almacenara los datos a guardar. Deberas pasar esto a su
  * debida funcion.
@@ -103,8 +102,7 @@ static void __create_table__(const char *query);
  * @param rows La cantidad de datos por fila 
  * 
  */
-bool __insert_into__(struct users_to_insert *const users_to_insert,
-                     struct products *const products);
+bool __insert_into__(struct users_to_insert *const users_to_insert);
 /**
  * @brief Recibe una query para ejecutar la consulta.
  * 
@@ -130,52 +128,6 @@ static char *allocate_str(int len);
  */
 static void *__get_name__(const unsigned id);
 
-/**
- * @brief Devuelve el precio del producto.
- * 
- * @param id ID del producto.
- * @return int El precio del producto.
- */
-static void *__get_price__(const unsigned id);
-
-/**
- * @brief Devuelve la cantidad actual del producto en inventario.
- * 
- * @param id ID del producto.
- * @return int Cantidad disponible.
- */
-static void *__get_quantity__(const unsigned id);
-
-/**
- * @brief update devuelve esta funcion.
- * 
- * @param id ID del producto a modificar.
- * @param new_name El nuevo nombre que se le pondra al producto.
- * @return true La modificacion fue un exito.
- * @return false Fallo al modificar.
- */
-static bool __update_name__(const unsigned id, const char *new_name);
-
-/**
- * @brief update devuelve esta funcion.
- * 
- * @param id ID del producto a editar.
- * @param quantity Cantidad que sera sumanda o restada.
- * @return true La modificacion fue un exito.
- * @return false La modifiacion fallo.
- */
-static bool __update_quantity__(const unsigned id, const int quantity);
-
-/**
- * @brief update devuelve esta funcion.
- * 
- * @param id ID del producto a modificar.
- * @param new_price El nuevo precio del producto.
- * @return true La modificacion fue un exito.
- * @return false La modifiacion fallo. 
- */
-static bool __update_price__(const unsigned id, const unsigned new_price);
-
 //! *-*-*-*-*-*-*- Metodos externos, pueden usarse sin problemas.*-*-*-*-*-*-*- 
 /**
  * @brief Anade a un nuevo usario a la database.
@@ -198,34 +150,12 @@ void add_user(const char *username, const char *password, int is_admin);
 extern int validate(const char *const username, const char *const password);
 
 /**
- * @brief Busca un producto por su id y muestra toda su informacion.
+ * @brief Para comentar.
  * 
- * @param id 
- * @return char* Devuelve la informacion recolectada.
+ * @return true 
+ * @return false 
  */
-extern char *search_product_by_id(const int id);
-
-/**
- * @brief Actualiza un valor. Ej: id = 10, new_value = 50, is_int = true;
- * Como new_value es un string, y queremos guardar un int debemos pasar is_int = true.
- * El producto con el id = 10 sele asiganara un nuevo valor (50).
- * 
- * IMPORTANT: Para uso interno.
- * IMPORTANT: Si no vas a actualizar algun parametro pasa Null. Ej. 5, NULL...
- * 
- * TODO: Abstracion para users.
- * 
- * @param id Identificador exclusivo (no se puede repetir, y se genera 
- * automaticamente) para cada producto. NO puede ser NULL.
- * @param new_name Nuevo nombre para el producto con el id suministrado.
- * @param new_sellPrice Nuevo sell_price para el producto con el id suministrado.
- * @param new_availableQuantity Nuevo available_quantity para el producto con el
- * id suministrado.
- * @return true Se han asignado los nuevos valores succesfully!
- * @return false No se han podido asignar los nuevos valores.
- */
-bool update(const unsigned id, const char *new_name,
-            const unsigned *new_sellPrice, const int *new_availableQuantity);
+bool update();
 
 /**
  * @brief Devolvera el valor de una columna. El retorna debe ser casteado con el
