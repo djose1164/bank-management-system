8053137938d7f5dff73ec9b0f6eafcb14a248bd3
Jose S. Daniel <djose1164@gmail.com>
2021-04-23 00:07:10 -0400

    Add base.h

---
+++ include/base.h
@@ -0,0 +1,15 @@
+#ifndef BASE_H
+#define BASE_H
+
+/**
+ * @brief Unformacion compartida entre el client y el bank.
+ * 
+ */
+struct Base
+{
+    /**El dinero disponible */
+    double available_cash;
+};
+
+
+#endif //BASE_H
