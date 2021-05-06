7b4254152e0985b0c9a49694b9a8348d6c2c2505
Jose S. Daniel <djose1164@gmail.com>
2021-05-06 14:01:10 -0400

    Login and sign up works.

--- src/main.py
+++ src/main.py
@@ -11,7 +11,10 @@ import kivy
 kivy.require("1.11.1")
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
-from database import Database
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.properties import ObjectProperty
+from database import db
 
 # CPython.
 from ctypes import CDLL
@@ -20,11 +23,22 @@ from ctypes import CDLL
 # This' the first screen (1)
 # This screen will display the login. Both, signup and login options.
 class LoginScreen(Screen):
+    username = ObjectProperty(None)
+    password = ObjectProperty(None)
 
     def login(self):
-        dir_path = "../src/login.so"
-        sm.current = "menu"
-        print("Holaaa")
+        if db.validate_user(self.username.text, self.password.text):
+            sm.current = "menu"
+        else:
+            popup = Popup(
+                title="Error!",
+                content=Label(
+                    text="Ups! It seems like you haven't created any "
+                    + "account yet\nTry to create a new one first!"
+                ),
+                size_hint=(0.5, 0.4),
+            )
+            popup.open()
 
     def sign_up(self):
         sm.current = "sign_up"
@@ -38,7 +52,35 @@ class SignupScreen(Screen):
         Screen (Screen): A different screen for signing up.
     """
 
-    pass
+    username = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def add_new_user(self):
+        if db.create_new_user(self.username.text, self.password.text):
+            popup = Popup(
+                title="Done!",
+                content=Label(text="User created succefully!", bold=True),
+                size_hint=(0.5, 0.4),
+                on_dismiss = self.go_to_menu
+            )
+
+            popup.open()
+        else:
+            Popup(
+                title="Error!",
+                content=Label(
+                    text="Ups! We've caught a bug!\nPlease send an issue with"
+                    + " an extend description of this annoying bug!",
+                    italic=True,
+                    font_size=20,
+                    halign="justify",
+                ),
+                size_hint=(0.8, 0.5),
+            ).open()
+            
+    def go_to_menu(self, instance):
+        print(f"{instance}")
+        sm.current = "menu"
 
 
 # This' the second screen (2)
