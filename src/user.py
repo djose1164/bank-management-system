8209c2a695dc32f71fb6b2cb342d1741c0ad22f3
Jose S. Daniel <djose1164@gmail.com>
2021-05-08 02:37:29 -0400

    Added some documantation.

--- src/user.py
+++ src/user.py
@@ -1,13 +1,23 @@
 """This module -user- will contain the neccessary methods of an user
 for interacting with the bank.
 """
-
+from database import db
 
 class Person:
-    def __init__(self, name, direction, phone_number):
-        self._name = name
-        self._direction = direction
-        self._phone_number = phone_number
+    """Base class for User. Here are implemented some basic methods for
+    inheretance.
+    """
+    def __init__(self, name: str, direction: str, phone_number: int):
+        """Create a new person with some required attribute.
+
+        Args:
+            name (str): The person's name.
+            direction (str): The person's direction.
+            phone_number (int): The peron's phone number.
+        """
+        self._name: str = name
+        self._direction: str = direction
+        self._phone_number: int = phone_number
 
     def get_name(self):
         return self._name
@@ -27,15 +37,23 @@ class Person:
 
 
 class User(Person):
-    def __init__(self, name, direction, phone_number, username, password):
+    def __init__(
+        self, name: str, direction: str, phone_number, 
+        username: str, password: str
+    ):
         super().__init__(name, direction, phone_number)
-        self._username = username
-        self.__password = password
-        
+        self._username: str = username
+        self.__password: str = password
+
     def get_username(self):
         return self._username
-    
+
     def get_full_info(self):
-        data_show = super().get_full_info()
+        """Get the full info from Person class and add to it the username.
+
+        Returns:
+            list: A list containing the full info of this user.
+        """
+        data_show: list = super().get_full_info()
         data_show.append(self._username)
         return data_show
