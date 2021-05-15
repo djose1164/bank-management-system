0a3e172c841a178a857897591f625a9a42c7d2ee
Jose S. Daniel <djose1164@gmail.com>
2021-05-15 01:34:12 -0400

    User object working.

--- src/user.py
+++ src/user.py
@@ -1,29 +1,33 @@
 """This module -user- will contain the neccessary methods of an user
 for interacting with the bank.
 """
-from database import db
+import ctypes
+import database
+
+
 
 class Person:
     """Base class for User. Here are implemented some basic methods for
     inheretance.
     """
-    def __init__(self, name: str, direction: str, phone_number: int):
+
+    def __init__(self, name: str, address: str, phone_number: int):
         """Create a new person with some required attribute.
 
         Args:
             name (str): The person's name.
-            direction (str): The person's direction.
+            address (str): The person's address.
             phone_number (int): The peron's phone number.
         """
         self._name: str = name
-        self._direction: str = direction
+        self._address: str = address
         self._phone_number: int = phone_number
 
     def get_name(self):
         return self._name
 
-    def get_direction(self):
-        return self._direction
+    def get_address(self):
+        return self._address
 
     def get_phone_number(self):
         return self._phone_number
@@ -31,29 +35,33 @@ class Person:
     def get_full_info(self):
         return [
             self._name,
-            self._direction,
+            self._address,
             self._phone_number,
         ]
 
 
-class User(Person):
-    def __init__(
-        self, name: str, direction: str, phone_number, 
-        username: str, password: str
-    ):
-        super().__init__(name, direction, phone_number)
+class User:
+    """For the manage of the bank."""
+
+    def __init__(self, username: str, password: str):
         self._username: str = username
         self.__password: str = password
+        self._id = database.db.load_id(username, password)
+        print(f"## id: {self._id}")
 
+    @property
     def get_username(self):
         return self._username
-
-    def get_full_info(self):
-        """Get the full info from Person class and add to it the username.
-
-        Returns:
-            list: A list containing the full info of this user.
-        """
-        data_show: list = super().get_full_info()
-        data_show.append(self._username)
-        return data_show
+    
+    @property
+    def get_password(self):
+        return self.__password
+    
+    @property
+    def get_id(self):
+        return self._id
+    
+user = None
+def init_user(username, password):
+    global user
+    user = User(username, password)
