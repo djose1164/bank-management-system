150425deb6107db771969a3e2ad2db518f946dc8
Jose S. Daniel <djose1164@gmail.com>
2021-05-23 19:03:03 -0400

    Adds todo with the bug to fix.

--- src/calc.py
+++ src/calc.py
@@ -9,6 +9,7 @@ from requests.models import Response
 
 digits: int = 2
 
+# TODO: Fix request to API error.
 
 def _do_connection_to_api(_from: str, to: str):
     BASE_URL: str = "https://free.currconv.com/api/v7/"
@@ -39,16 +40,13 @@ def get_dop_to_eur():
     response = _do_connection_to_api("DOP", "EUR")
     return response.json()["DOP_EUR"]
 
-
 def get_eur_to_dop():
     response = _do_connection_to_api("EUR", "DOP")
     return response.json()["EUR_DOP"]
 
-
 def get_usd_to_dop():
     response = _do_connection_to_api("USD", "DOP")
     return response.json()["USD_DOP"]
-
 
 def dollars_to_euros(amount: float):
     converted_value: float = amount * get_usd_to_eur()
@@ -69,12 +67,11 @@ def dop_to_euros(amount: float):
     converted_value: float = amount * get_dop_to_eur()
     return round(converted_value, digits)
 
-
 def dollars_to_dop(amount: float):
     converted_value: float = amount * get_usd_to_dop()
     return round(converted_value, digits)
 
-
 def euros_to_dop(amount: float):
     converted_value: float = amount * get_eur_to_dop()
     return round(converted_value, digits)
+
